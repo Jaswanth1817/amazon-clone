100533f8d3b0f5c8e0ba63d5ba4c482b50adfddc
KONGARA JASWANTH <kongarajaswanth4@gmail.com>
2022-02-25 11:10:34 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCr8_FaVHquvgGhUO0T9BMEsHq6LJs46yk",
-  authDomain: "clone-df25a.firebaseapp.com",
-  projectId: "clone-df25a",
-  storageBucket: "clone-df25a.appspot.com",
-  messagingSenderId: "275153896021",
-  appId: "1:275153896021:web:da41855ff1ceb48d9ccadc",
-  measurementId: "G-JLSCJVFM0W"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
